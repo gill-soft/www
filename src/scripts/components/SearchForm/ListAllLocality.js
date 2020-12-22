7cfb189916480c38202c85764b17ffee8250b8a7
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2020-12-22 12:58:21 +0200

    add functional for inputs

--- src/scripts/components/SearchForm/ListAllLocality.js
+++ src/scripts/components/SearchForm/ListAllLocality.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 
+export default class ListAllLocality extends Component {
+  state = {
+    dataFilter: [],
+  };
+  componentDidUpdate(prevProps) {
+    if (prevProps.filterData !== this.props.filterData)
+      this.setState({ dataFilter: this.props.filterData });
+  }
+
+  getVal = ({ target }) => {
+    this.props.getValue(target.textContent);
+    this.props.isVisibleList();
+  };
+  render() {
+    const { dataFilter } = this.state;
+    const { data, classToggle } = this.props;
+    return (
+      <ul className={`listLocality ${classToggle}`} onClick={this.getVal}>
+        {dataFilter.length > 0
+          ? dataFilter.map(item => (
+              <ListItem key={item.id} item={item} data={data} />
+            ))
+          : data.map(item => (
+              <ListItem key={item.id} item={item} data={data} />
+            ))}
+      </ul>
+    );
+  }
+}
+
 class ListItem extends Component {
   state = {
     locality: '',
   };
 
   componentDidMount() {
-    const { item } = this.props;
-    if (item.type === 'LOCALITY') {
+    const { item, filterData } = this.props;
+    if (item.type === 'LOCALITY')
       this.setState({ locality: this.getLocality(item) });
-    }
   }
 
   getLocality(item) {
-    return `<span class="www">${item.name.RU}</span> - ${this.full(
+    return `<span class="www">${item.name.EN}</span> - ${this.full(
       item,
       item.parent.id,
     )}`;
@@ -23,10 +52,10 @@ class ListItem extends Component {
 
     if (!obj.parent.id) return;
     const [result] = data.filter(el => el.id === id);
-    if (!result.parent) {
-      return result.name.RU;
+    if (!result?.parent) {
+      return result.name.EN;
     } else {
-      return result.name.RU + '/' + this.full(result, result.parent.id);
+      return result.name.EN + '/' + this.full(result, result.parent.id);
     }
   }
   createMarkup() {
@@ -38,20 +67,5 @@ class ListItem extends Component {
     const { item, getValue } = this.props;
     const { locality, fullLocality } = this.state;
     return <li dangerouslySetInnerHTML={this.createMarkup()} />;
-  }
-}
-
-export default class ListAllLocality extends Component {
-  getVal = ({ target }) => {
-    this.props.getValue(target.textContent);
-  };
-  render() {
-    const { data } = this.props;
-    return (
-      <ul className="listLocality" onClick={this.getVal}>
-        {data.length > 0 &&
-          data.map(item => <ListItem key={item.id} item={item} data={data} />)}
-      </ul>
-    );
   }
 }
--- src/scripts/components/SearchForm/SearchForm.js
+++ src/scripts/components/SearchForm/SearchForm.js
@@ -8,33 +8,86 @@ export default class SearchForm extends Component {
     inputValueWhereTo: '',
     inputDate: '',
     data: [],
+    filterData: [],
     isVisible: false,
+    classToggle: '',
+    
   };
   componentDidMount() {
     getAllStops()
       .then(({ data }) => this.setState({ data: data }))
       .catch(err => console.log(err));
   }
+  componentDidUpdate(prevPops, prevState) {
+    if (prevState.inputValueFrom !== this.state.inputValueFrom)
+      this.felterData(this.state.inputValueFrom);
+    if (prevState.inputValueWhereTo !== this.state.inputValueWhereTo)
+      this.felterData(this.state.inputValueWhereTo);
+  }
 
-  // getSearchRouts = () => {};
+  felterData = value => {
+    const {data} = this.state
+    const newData = data.filter(item => {
+      if (item.type === 'LOCALITY') {
+        return (
+          item.name.EN.toLowerCase().indexOf(value.trim().toLowerCase()) > -1
+        );
+      }
+    });
+    this.setState({filterData: newData})
+  };
+ 
+  // ======== получаем значения в input ==================
   handleChange = ({ target }) => {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
     this.setState({ [name]: value });
   };
 
+  handleClickInput = ({ target }) => {
+    this.state.isVisible === false ? this.isVisibleList() : null;
+
+    switch (target.name) {
+      case 'inputValueFrom':
+        this.setState({ classToggle: 'from' });
+        break;
+      case 'inputValueWhereTo':
+        this.setState({ classToggle: 'whereTo' });
+        break;
+      default:
+        null;
+    }
+
+  };
+
+  isVisibleList = () => {
+    this.setState(prev => ({ isVisible: !prev.isVisible }));
+  };
+
   getValue = val => {
-    this.setState({ inputValueFrom: val });
+    if (this.state.classToggle === 'from')
+      this.setState({ inputValueFrom: val });
+    if (this.state.classToggle === 'whereTo')
+      this.setState({ inputValueWhereTo: val });
+  };
+
+  changeButton = () => {
+    const from = this.state.inputValueFrom;
+    const whereTo = this.state.inputValueWhereTo;
+    this.setState({ inputValueFrom: whereTo });
+    this.setState({ inputValueWhereTo: from });
   };
 
   render() {
-    console.log(this.state);
+
     const {
       inputValueFrom,
       inputValueWhereTo,
       inputDate,
       data,
       isVisible,
+      classToggle,
+      filterData
     } = this.state;
     return (
       <>
@@ -43,13 +96,18 @@ export default class SearchForm extends Component {
             name="inputValueFrom"
             value={inputValueFrom}
             onChange={this.handleChange}
-            onClick={() => this.setState({ isVisible: true })}
+            onClick={this.handleClickInput}
+            autoComplete="off"
           ></input>
+          <button type="button" onClick={this.changeButton}>
+            &hArr;
+          </button>
           <input
             name="inputValueWhereTo"
             value={inputValueWhereTo}
             onChange={this.handleChange}
-            onClick={() => this.setState({ isVisible: true })}
+            onClick={this.handleClickInput}
+            autoComplete="off"
           ></input>
           <input
             name="inputDate"
@@ -61,7 +119,15 @@ export default class SearchForm extends Component {
             search
           </button>
         </form>
-        {isVisible && <ListAllLocality data={data} getValue={this.getValue} />}
+        {isVisible && (
+          <ListAllLocality
+            data={ data }
+            filterData={filterData}
+            getValue={this.getValue}
+            classToggle={classToggle}
+            isVisibleList={this.isVisibleList}
+          />
+        )}
       </>
     );
   }
