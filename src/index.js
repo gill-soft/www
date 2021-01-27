f64256ac93d04e3934729f85c8701344f5174e03
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2021-01-27 16:10:04 +0200

    predeploy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./stylesheet/main.css";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import LanguageSelect from "./components/Language/LanguageSelect";
-import App from './components/App'
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,15 +19,16 @@ ReactDOM.render(
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <Provider store={store}>
         <Route component={App} />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
   document.getElementById("trips")
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
