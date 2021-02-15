39d2610733e9878656fb91f0fb10334383c6b0ec
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2021-02-15 17:00:52 +0200

    translate OrderPage

--- src/components/Order/FormForBuy.js
+++ src/components/Order/FormForBuy.js
@@ -6,6 +6,9 @@ import { toBook } from "../../services/api";
 import { getError, startLoader, stopLoader } from "../../redux/global/globalActions";
 import PublickOffer from "../PublickOffer/PublickOffer";
 import Modal from "../Modal/Modal";
+import Loader from "../Loader/Loader";
+import { IntlProvider, FormattedMessage } from "react-intl";
+import { messages } from "../../intl/OrderPageMessanges";
 
 class FormForBuy extends Component {
   state = {
@@ -13,7 +16,7 @@ class FormForBuy extends Component {
     email: "",
     resp: {},
     isOffer: true,
-    isPersonal: true
+    isPersonal: true,
   };
 
   componentDidMount() {
@@ -125,123 +128,139 @@ class FormForBuy extends Component {
   };
 
   render() {
-    const { values, email, isModal, isPersonal, isOffer} = this.state;
-    const { isLoading } = this.props;
+    const { values, email, isModal, isPersonal, isOffer } = this.state;
+    const { isLoading, lang } = this.props;
+    const locale = lang === "UA" ? "UK" : lang;
+
     return (
-      <div className={styles.container}>
-        {/* <pre>{JSON.stringify(this.state, null, 4)}</pre> */}
-        <h3 className={styles.title}>Данные о пассажирах*:</h3>
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <div className={styles.container}>
+          {/* <pre>{JSON.stringify(this.state, null, 4)}</pre> */}
 
-        <form onSubmit={this.handleSubmit}>
-          {values.length > 0 &&
-            values.map((el, idx) => {
-              return (
-                <div className={styles.box} key={idx}>
-                  <div className={styles.svgBox}>
-                    {" "}
-                    <Person className={styles.svg} fill="var(--color-secondary)" />
-                    <span>{idx + 1}</span>
+          <h3 className={styles.title}>
+            <FormattedMessage id="title" />
+          </h3>
+
+          <form onSubmit={this.handleSubmit}>
+            {values.length > 0 &&
+              values.map((el, idx) => {
+                return (
+                  <div className={styles.box} key={idx}>
+                    <div className={styles.svgBox}>
+                      {" "}
+                      <Person className={styles.svg} fill="var(--color-secondary)" />
+                      <span>{idx + 1}</span>
+                    </div>
+                    <div className={styles.inputBox}>
+                      <input
+                        className={styles.input}
+                        name="name"
+                        type="text"
+                        value={this.getValueName(el.id)}
+                        onChange={(e) => this.handleChange(el.id, e)}
+                        placeholder="name"
+                        autoComplete="off"
+                        required={true}
+                      />
+                      <input
+                        className={styles.input}
+                        name="surname"
+                        type="text"
+                        value={this.getValueSurname(el.id)}
+                        onChange={(e) => this.handleChange(el.id, e)}
+                        placeholder="surname"
+                        autoComplete="off"
+                        required={true}
+                      />
+                      <input
+                        className={styles.input}
+                        name="phone"
+                        type="tel"
+                        value={this.getValuePhone(el.id)}
+                        onChange={(e) => this.handleChange(el.id, e)}
+                        placeholder="phone**"
+                        autoComplete="nope"
+                        required={true}
+                      />
+                      <p className={styles.price}>{this.props.price} грн</p>
+                      <button
+                        className={styles.buttonRemove}
+                        type="button"
+                        name={el.id}
+                        onClick={this.handleRemove}
+                        disabled={values.length <= 1 ? true : false}
+                      ></button>
+                    </div>
                   </div>
-                  <div className={styles.inputBox}>
-                    <input
-                      className={styles.input}
-                      name="name"
-                      type="text"
-                      value={this.getValueName(el.id)}
-                      onChange={(e) => this.handleChange(el.id, e)}
-                      placeholder="name"
-                      autoComplete="off"
-                      required={true}
-                    />
-                    <input
-                      className={styles.input}
-                      name="surname"
-                      type="text"
-                      value={this.getValueSurname(el.id)}
-                      onChange={(e) => this.handleChange(el.id, e)}
-                      placeholder="surname"
-                      autoComplete="off"
-                      required={true}
-                    />
-                    <input
-                      className={styles.input}
-                      name="phone"
-                      type="tel"
-                      value={this.getValuePhone(el.id)}
-                      onChange={(e) => this.handleChange(el.id, e)}
-                      placeholder="phone**"
-                      autoComplete="nope"
-                      required={true}
-                    />
-                    <p className={styles.price}>{this.props.price} грн</p>
-                    <button
-                      className={styles.buttonRemove}
-                      type="button"
-                      name={el.id}
-                      onClick={this.handleRemove}
-                      disabled={values.length <= 1 ? true : false}
-                    ></button>
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
 
-          <button
-            className={styles.buttonAdd}
-            type="button"
-            onClick={this.handleAdd}
-            disabled={values.length >= 10 ? true : false}
-          >
-            Добавить пассажира
-          </button>
-          <h3 className={styles.customer}>Данные о покупателе*:</h3>
-
-          <input
-            className={styles.input}
-            type="email"
-            name="email"
-            value={email}
-            placeholder="email"
-            autoComplete="nope"
-            onChange={this.handleChangeEmail}
-            required={true}
-          />
-          <div className={styles.publicOfferBox}>
+            <button
+              className={styles.buttonAdd}
+              type="button"
+              onClick={this.handleAdd}
+              disabled={values.length >= 10 ? true : false}
+            >
+              <FormattedMessage id="buttonAdd" />
+            </button>
+            <h3 className={styles.customer}>
+              <FormattedMessage id="customer" />
+            </h3>
             <input
-              name="publicOffer"
-              type="checkbox"
-              onChange={() => this.setState({ isOffer: !this.state.isOffer })}
-            />{" "}
-            <p>
-              Я принимаю условия <span onClick={this.openModal}>публичной оферты</span>
-            </p>
-          </div>
-          <div className={styles.publicOfferBox}>
-            <input
-              name="personalData"
-              type="checkbox"
-              onChange={() => this.setState({ isPersonal: !this.state.isPersonal })}
-            />{" "}
-            <p>
-              Я даю согласие на  <span onClick={this.openModal}>обработку персональных данных</span>
-            </p>
-          </div>
+              className={styles.input}
+              type="email"
+              name="email"
+              value={email}
+              placeholder="email"
+              autoComplete="nope"
+              onChange={this.handleChangeEmail}
+              required={true}
+            />
+            <div className={styles.publicOfferBox}>
+              <input
+                name="publicOffer"
+                type="checkbox"
+                onChange={() => this.setState({ isOffer: !this.state.isOffer })}
+              />{" "}
+              <p>
+                <FormattedMessage id="offer" />
+                <span onClick={this.openModal}>
+                  <FormattedMessage id="offerSpan" />
+                </span>
+              </p>
+            </div>
+            <div className={styles.publicOfferBox}>
+              <input
+                name="personalData"
+                type="checkbox"
+                onChange={() => this.setState({ isPersonal: !this.state.isPersonal })}
+              />{" "}
+              <p>
+                <FormattedMessage id="myData" />
+                <span onClick={this.openModal}>
+                  <FormattedMessage id="myDataSpan" />
+                </span>
+              </p>
+            </div>
+            <button
+              className={styles.buttonBuy}
+              type="submit"
+              disabled={isOffer || isPersonal}
+            >
+              {isLoading ? <Loader /> : <FormattedMessage id="buy" />}
+            </button>
+          </form>
 
-          <button
-            className={styles.buttonBuy}
-            type="submit"
-            disabled={isOffer || isPersonal}
-          >
-            {isLoading ? "Loading..." : "Перейти к оплате"}
-          </button>
-        </form>
-        <p className={styles.text}>*Все поля обязательны к заполнению</p>
-        <p className={styles.text}>
-          **Укажите актуальный номер телефона для связи в случае изменений рейса.
-        </p>
-        {isModal && <Modal onClose={this.closeModal} component={<PublickOffer />} />}
-        <pre>{JSON.stringify(this.state.resp, null, 4)}</pre>
-      </div>
+          <p className={styles.text}>
+            <FormattedMessage id="requared" />
+          </p>
+          <p className={styles.text}>
+            <FormattedMessage id="numTel" />
+          </p>
+          {isModal && <Modal onClose={this.closeModal} component={<PublickOffer />} />}
+          <pre>{JSON.stringify(this.state.resp, null, 4)}</pre>
+        </div>
+      </IntlProvider>
     );
   }
 }
--- src/components/Order/OrderInfo.js
+++ src/components/Order/OrderInfo.js
@@ -1,35 +1,51 @@
 import { connect } from "react-redux";
 import React from "react";
 import styles from "./OrderInfo.module.css";
+import { IntlProvider, FormattedMessage } from "react-intl";
+import { messages } from "../../intl/OrderPageMessanges";
 
 const OrderInfo = ({
-  from,
   to,
+  from,
+  lang,
   price,
-  departureDate,
-  arrivalDate,
-  fromStop,
-  toStop,
   total,
+  toStop,
+  fromStop,
+  arrivalDate,
+  departureDate,
 }) => {
+  const locale = lang === "UA" ? "UK" : lang;
+
   const getTotalPrice = () => {
     return (total * price).toFixed(2);
   };
   return (
-    <div>
-      <h3 className={styles.title}>Детали заказа</h3>
-      <div className={styles.orderBox}>
-        <h4 className={styles.subTitle}>Отправление</h4>
-        <p className={styles.locality}>{from}</p>
-        <p className={styles.localityStop}>{fromStop}</p>
-        <p className={styles.date}>{departureDate}</p>
-        <h4 className={styles.subTitle}>Прибытие</h4>
-        <p className={styles.locality}>{to}</p>
-        <p className={styles.localityStop}>{toStop}</p>
-        <p className={styles.date}>{arrivalDate}</p>
-        <p className={styles.total}>К оплате: {getTotalPrice()} грн</p>
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <div>
+        <h3 className={styles.title}>
+          <FormattedMessage id="details" />
+        </h3>
+        <div className={styles.orderBox}>
+          <h4 className={styles.subTitle}>
+            <FormattedMessage id="departure" />
+          </h4>
+          <p className={styles.locality}>{from}</p>
+          <p className={styles.localityStop}>{fromStop}</p>
+          <p className={styles.date}>{departureDate}</p>
+          <h4 className={styles.subTitle}>
+            <FormattedMessage id="arrival" />
+          </h4>
+          <p className={styles.locality}>{to}</p>
+          <p className={styles.localityStop}>{toStop}</p>
+          <p className={styles.date}>{arrivalDate}</p>
+          <p className={styles.total}>
+            <FormattedMessage id="pay" />
+            {getTotalPrice()} грн
+          </p>
+        </div>
       </div>
-    </div>
+    </IntlProvider>
   );
 };
 const mapStateToProps = (state) => ({
@@ -40,6 +56,7 @@ const mapStateToProps = (state) => ({
   departureDate: state.order.order.departureDate,
   arrivalDate: state.order.order.arrivalDate,
   price: state.order.order.price,
+  lang: state.language,
 });
 
 export default connect(mapStateToProps)(OrderInfo);
