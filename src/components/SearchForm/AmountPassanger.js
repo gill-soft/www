801bbcff3eab5bcfc619000dee3e8d119889a56d
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2021-02-23 12:43:20 +0200

    change amount redux

--- src/components/SearchForm/AmountPassanger.js
+++ src/components/SearchForm/AmountPassanger.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./AmountPassanger.module.css";
 import {
   decrementAmountPassangers,
@@ -10,50 +10,49 @@ import { ReactComponent as Minus } from "../../images/remove-black-18dp.svg";
 import { IntlProvider, FormattedMessage } from "react-intl";
 import { messages } from "../../intl/HomePageMessanges";
 
-const AmountPassanger = ({ amount, increment, decrement, lang }) => {
+const AmountPassanger = () => {
+  const dispatch = useDispatch();
+  const decrement = useCallback(() => {
+    dispatch(decrementAmountPassangers());
+  }, [dispatch]);
+  const increment = useCallback(() => {
+    dispatch(incrementAmountPassangers());
+  }, [dispatch]);
+  const amount = useSelector((state) => state.searchForm.amountPassanger);
+  const lang = useSelector((state) => state.language);
   const locale = lang === "UA" ? "UK" : lang;
 
   return (
     <IntlProvider locale={locale} messages={messages[locale]}>
-
-    <div className={styles.amountBox}>
-    
-      <p><FormattedMessage id="inputAmount" />: </p>
-      <div className={styles.flex}>
-        <button
-          type="button"
-          className={styles.button}
-          name="decrement"
-          disabled={amount <= 1}
-          onClick={() => decrement()}
-        >
-          {" "}
-          <Minus />{" "}
-        </button>
-        <span>{amount}</span>
-        <button
-          type="button"
-          className={styles.button}
-          name="increment"
-          disabled={amount >= 10}
-          onClick={() => increment()}
-        >
-          <Plus />
-        </button>
+      <div className={styles.amountBox}>
+        <p>
+          <FormattedMessage id="inputAmount" />:{" "}
+        </p>
+        <div className={styles.flex}>
+          <button
+            type="button"
+            className={styles.button}
+            name="decrement"
+            disabled={amount <= 1}
+            onClick={decrement}
+          >
+            {" "}
+            <Minus />{" "}
+          </button>
+          <span>{amount}</span>
+          <button
+            type="button"
+            className={styles.button}
+            name="increment"
+            disabled={amount >= 10}
+            onClick={increment}
+          >
+            <Plus />
+          </button>
+        </div>
       </div>
-    </div>
     </IntlProvider>
   );
 };
 
-const mapStateToProps = (state) => ({
-  amount: state.searchForm.amountPassanger,
-  lang: state.language,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  increment: () => dispatch(incrementAmountPassangers()),
-  decrement: () => dispatch(decrementAmountPassangers()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(AmountPassanger);
+export default AmountPassanger;
